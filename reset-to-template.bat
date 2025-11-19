3c7d1f25753a95107f23b3f167f95eabcfa73260
xXseesXx <fabibeer2004@gmail.com>
2025-11-20 00:58:53 +0100

    Clean up template files

--- reset-to-template.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0reset-to-template.ps1"
-pause
--- setup-new-mod.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0setup-new-mod.ps1"
-pause
